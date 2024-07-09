16ab802af6affe659f52f940fc4b65d0b083846e
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-09 10:54:01 +0100

    Added more configuration to app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import path from 'path';
 import router from './router';
@@ -5,7 +6,16 @@ import router from './router';
 const app = express();
 const port = 3000;
 
+// Data parsing
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Set ejs templating view engine
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+
+// App router
 app.use('/', router);
 
 app.listen(port, () => {
