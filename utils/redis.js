e1e8e6d974a8a2fdb002f6ede2decc69bfaa79e6
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-09 10:52:55 +0100

    redis client handler

--- utils/redis.js
+++ utils/redis.js
@@ -24,28 +24,35 @@ class RedisClient {
   async getRoomMembers(roomId) {
     try {
       const roomData = await this.hgetallAsync(roomId);
-      console.log(`Members of ${roomId} - ${Json.stringify(roomData)}`);
+      console.log(`Members of ${roomId} - ${JSON.stringify(roomData)}`);
       return roomData;
     } catch (err) {
-      console.error(err);
+      console.error(err.message);
     }
   }
   // Add a new member to the room
   // Takes 3 args: roomID (str), username, userData (json string), duration (in seconds)
   async setRoomMember(roomId, username, userData, duration) {
     try {
-      await this.client.hsetAsync(roomId, username, userData);
-      await this.client.expireAsync(roomId, 60);
+      await this.hsetAsync(roomId, username, userData);
+      if (duration) {
+        try {
+          await this.expireAsync(roomId, duration);
+        } catch (err) {
+          console.log('Error setting room TTL');
+        }
+      }
       console.log(`Added ${username} to room ${roomId}`);
     } catch (err) {
       console.error(`An error occured while adding ${username} to the room.\n${err}`);
+      throw err;
     }
   }
 
   // Deletes room member information
   async delRoomMember(roomId, username) {
     try {
-      await this.client.hdelAsync(roomId, username);
+      await this.hdelAsync(roomId, username);
     } catch (err) {
       console.error(`Error occurred while removing member - ${err}`);
     }
@@ -54,7 +61,7 @@ class RedisClient {
   // Deletes room information
   async delRoom(roomId) {
     try {
-      await this.client.hdelAsync(roomId);
+      await this.delAsync(roomId);
     } catch (err) {
       console.error(`Error occurred while deleting the room - ${err}`);
     }
