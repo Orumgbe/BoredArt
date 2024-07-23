d3fba50df26dd40c85319f924b34f0ff823ff02f
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-23 08:55:35 +0100

    indicator for when player is choosing word

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -35,4 +35,12 @@ document.addEventListener("DOMContentLoaded", function() {
     chatList.appendChild(item);
     chatList.scrollTop = chatList.scrollHeight;
   });
+
+  socket.on('player-choosing', (username) => {
+    const item = document.createElement('li');
+    item.textContent = `${username} is choosing a word`;
+    item.id = 'game-info-text';
+    chatList.appendChild(item);
+    chatList.scrollTop = chatList.scrollHeight;
+  });
 });
