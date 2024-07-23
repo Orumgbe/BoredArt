7505b269c43992cfc2a7b5f4e0d372c6d0a6f092
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-23 08:56:58 +0100

    added function for updating socket.id with api

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,3 +1,63 @@
+const domainName = 'http://0.0.0.0:3000';
+
+async function getUser(roomId, username) {
+  try {
+    const res = await fetch(`${domainName}/api/${roomId}/${username}`);
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const userData = await res.json();
+    return userData;
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+    return {};
+  }
+}
+
+async function getAllUsers(roomId) {
+  try {
+    const res = await fetch(`${domainName}/api/${roomId}`);
+    if (!res.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const users = await res.json();
+    return users;
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+    return {};
+  }
+}
+
+async function updateSocketId(roomId, username, socketId) {
+  try {
+    const response = await fetch(`${domainName}/api/${roomId}/${username}/${socketId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    });
+
+    const data = await response.json();
+    if (response.ok) {
+      console.log(data);
+    } else {
+      console.error(data.error);
+    }
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+function addUserToPage(username, userData) {
+  console.log('Adding user to page:', username, userData); // Add log
+  const membersContainer = document.getElementById('members_container');
+  const memberDiv = document.createElement("div");
+  memberDiv.classList.add("member");
+  memberDiv.id = username;
+  memberDiv.textContent = `${username} (Score: ${userData.score})`;
+  membersContainer.appendChild(memberDiv);
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
   const urlPath = window.location.pathname.split('/');
   const roomId = urlPath[urlPath.length - 1];
@@ -8,6 +68,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const username = localStorage.getItem('username');
     console.log(username, 'connected to server with id:', socket.id);
     clearTimeout(inactiveTimeout);
+    await updateSocketId(roomId, username, socket.id) 
     socket.emit('joinRoom', { roomId, username });
     try {
       const userData = await getUser(roomId, username);
@@ -60,42 +121,3 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   });
 });
-
-
-function addUserToPage(username, userData) {
-  console.log('Adding user to page:', username, userData); // Add log
-  const membersContainer = document.getElementById('members_container');
-  const memberDiv = document.createElement("div");
-  memberDiv.classList.add("member");
-  memberDiv.id = username;
-  memberDiv.textContent = `${username} (Score: ${userData.score})`;
-  membersContainer.appendChild(memberDiv);
-}
-
-export async function getUser(roomId, username) {
-  try {
-    const res = await fetch(`http://0.0.0.0:3000/api/${roomId}/${username}`);
-    if (!res.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const userData = await res.json();
-    return userData;
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-    return {};
-  }
-}
-
-export async function getAllUsers(roomId) {
-  try {
-    const res = await fetch(`http://0.0.0.0:3000/api/${roomId}`);
-    if (!res.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const users = await res.json();
-    return users;
-  } catch (error) {
-    console.error('There has been a problem with your fetch operation:', error);
-    return {};
-  }
-}
