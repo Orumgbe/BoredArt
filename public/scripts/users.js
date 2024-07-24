c0e043338afb30963624d9c7e9cd78ddba8dd0b5
Orumgbe <orumgbeugbekile@gmail.com>
2024-07-24 11:26:57 +0100

    display user count, activate leave button

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -62,6 +62,10 @@ document.addEventListener('DOMContentLoaded', async () => {
   const urlPath = window.location.pathname.split('/');
   const roomId = urlPath[urlPath.length - 1];
 
+
+  const memberCount = document.getElementById('member-count');
+  const maxCapacity = 5;
+  const leaveButton = document.getElementById('leaveButton');
   let inactiveTimeout;
 
   socket.on('connect', async () => {
@@ -79,6 +83,8 @@ document.addEventListener('DOMContentLoaded', async () => {
         existingMember.textContent = `${username}`;
       } else {
         const users = await getAllUsers(roomId);
+        // Add member count
+        memberCount.textContent = `${Object.keys(users).length}/${maxCapacity}`;
         users[username] = userData;
         for (const username in users) {
           addUserToPage(username, users[username]);
@@ -94,8 +100,55 @@ document.addEventListener('DOMContentLoaded', async () => {
     try {
       const userData = await getUser(roomId, user); // Pass roomId as well
       addUserToPage(user, userData);
+      const users = await getAllUsers(roomId);
+      // Update member count
+      memberCount.textContent = `${Object.keys(users).length}/${maxCapacity}`;
     } catch (error) {
       console.error('There has been a problem with your fetch operation:', error);
+    }
+  });
+
+  leaveButton.addEventListener('click', async () => {
+    // Request to clear the cookie
+    try {
+      const response = await fetch(`${domainName}/api/${roomId}/clear-cookie`, {
+        method: 'GET',
+        credentials: 'include', // Ensure cookies are included in the request
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to clear cookie');
+      }
+
+      // Disconnect from socket
+      socket.disconnect();
+
+      // Redirect to home page
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Error clearing cookie:', error);
+    }
+  });
+
+  leaveButton.addEventListener('touchstart', async () => {
+    // Request to clear the cookie
+    try {
+      const response = await fetch(`${domainName}/api/${roomId}/clear-cookie`, {
+        method: 'GET',
+        credentials: 'include', // Ensure cookies are included in the request
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to clear cookie');
+      }
+
+      // Disconnect from socket
+      socket.disconnect();
+
+      // Redirect to home page
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Error clearing cookie:', error);
     }
   });
 
@@ -111,6 +164,9 @@ document.addEventListener('DOMContentLoaded', async () => {
           if (!res.ok) {
             throw new Error('Network response was not ok');
           }
+          const users = await getAllUsers(roomId);
+          // Update member count after user leaves
+          memberCount.textContent = `${Object.keys(users).length}/${maxCapacity}`;
           const response = await res.json();
           console.log(response);
           disconnectedUser.remove();
